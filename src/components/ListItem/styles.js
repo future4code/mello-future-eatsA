59944398aad10fd679bd81ed40d44ca57f502137
Jonatan Machado <jonatanmachado35@gmail.com>
2020-07-14 16:12:35 -0300

    refactor: refactoring the cart page following the zeplin

--- src/components/ListItem/styles.js
+++ src/components/ListItem/styles.js
@@ -1,25 +1,71 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const View = styled.div`
+  display: flex;
   width: 328px;
   height: 112px;
+  border: 1px solid #b8b8b8;
   border-radius: 8px;
-  border: solid 1px #b8b8b8;
+  margin-bottom: 8px;
+`;
+
+export const Image = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+  width: 96px;
+  height: 112px;
+`;
+
+export const ContainerTop = styled.div`
+  width: 100%;
+  height: 70px;
+  /* background-color: blue; */
+`;
+export const ContainerDown = styled.div`
   display: flex;
-  flex-direction: row;
+  justify-content: space-between;
+  /* background-color: green; */
+  width: 100%;
+  height: 70px;
+`;
 
-  h2 {
-    color: #5cb646;
-    font-size: 16px;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: -0.39px;
-  }
+export const Conteudo = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* background-color: red; */
+  width: 70%;
+  height: 140px;
+  padding-left: 20px;
+  padding-top: 20px;
+`;
 
-  img {
-    width: 96px;
-    height: 112px;
+export const Titulo = styled.div`
+  font-size: 20px;
+  font-weight: 800;
+`;
+
+export const Descricao = styled.div``;
+export const Preco = styled.div``;
+export const Amount = styled.span`
+  margin: 0;
+  padding: 0;
+  margin-left: 186px;
+  margin-top: -20px;
+  border: 1px solid green;
+  border-radius: 8px;
+  p {
+    padding: 0;
+    margin: 0;
+    margin-left: 12px;
   }
+`;
+export const Adicionar = styled.button`
+  background: transparent;
+  border-radius: 8px;
+  margin-top: -10px;
+  width: 97px;
+  height: 33px;
+  padding: 0 12px 0 12px;
+  margin-right: -1px;
+  border: 1px solid #e02020;
 `;
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  MethodPayment,
+  Price,
+  Portage,
+  SubTitle,
+  TitleMethod,
+  PropertyDetails,
+  SubTitlePropertyDetails,
+} from './styles';
 
 import ListItem from '../../components/ListItem';
 
 function Cart() {
-  return <ListItem />;
+  return (
+    <>
+      <PropertyDetails>
+        <SubTitlePropertyDetails>
+          Bullguer Vila Madalena
+        </SubTitlePropertyDetails>
+        <p>R. Fradique Coutinho, 1136 - Vila Madalena</p>
+        <p>30 - 45 min</p>
+      </PropertyDetails>
+      <ListItem />
+      <ListItem />
+      <Portage>Frete R$ 6,00</Portage>
+      <SubTitle>SUBTOTAL</SubTitle>
+      <Price>R$ 67,00</Price>
+      <TitleMethod>forma de pagamento</TitleMethod>
+      <MethodPayment>
+        <label>
+          <input type="radio" name="method-payment" value="cash" />
+          Dinheiro
+        </label>
+        <label>
+          <input type="radio" name="method-payment" value="creditCard" />
+          Cartão de Crédito
+        </label>
+      </MethodPayment>
+      <button>Confirmar</button>
+    </>
+  );
 }
 
 export default Cart;

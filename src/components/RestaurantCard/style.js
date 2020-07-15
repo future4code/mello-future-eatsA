24103284bb39e3b2bbe839173ecc9d8bff8eb24c
Paulo Aguiar <paulopma@hotmail.com>
2020-07-15 18:28:53 -0300

    feat: adicionado condicional de cor no clique de categoria

--- src/components/RestaurantCard/style.js
+++ src/components/RestaurantCard/style.js
@@ -9,23 +9,23 @@ export const Main = styled.main`
   margin-top: 8px;
   border-radius: 8px;
   img {
-    width: 328px;
+    width: 326px;
     height: 120px;
     border-top-left-radius: 8px;
     border-top-right-radius: 8px;
     object-fit: cover;
   }
   .Restaurante {
-  margin: 8px 16px 4px 16px;
-  width: 296px;
-  font-family: Roboto;
-  font-size: 16px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: -0.39px;
-  color: #5cb646;
+    margin: 8px 16px 4px 16px;
+    width: 296px;
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    color: #5cb646;
   }
   div {
     margin: 0 16px 16px 16px;
